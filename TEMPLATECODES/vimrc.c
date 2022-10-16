21340aff4e610046c0db5e85583a53891956a0b0
JAYABHARATHI J <101325443+Jayabharathi7@users.noreply.github.com>
2022-10-16 11:55:32 +0530

    Update vimrc.c

--- TEMPLATECODES/vimrc.c
+++ TEMPLATECODES/vimrc.c
@@ -14,3 +14,50 @@ set sw=6
 filetype indent on
 set mouse=a
 autocmd BufNewFile *.c 0r ~/Templates/temp.c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Information
+Updates upto 2022
+UK singers - Male                                 Female                                                Actors                 Actress
+             Alan walker       Harry styles       Dua lipa     Jessie J          Clean bandit           Daniel Radcliffe       Keira knightley   
+             Ed sheeran        Zayn malik         Adele        Ellie goulding                           Robert pattinson       Rachel weisz
+             One Direction(5)                     Anne-Marie   Rita ora                                                        Kate winslet
+             
+             
+Paris singers -  Male           Actress
+                 DJ snake       Emma watson              
+                 
+USA singers - Male               Female                                                                   Actors               Actress
+              The chainsmokers   Taylor swift     (Ariana grande)   (Lady gaga)       (Cardi B)           Leonardo Dicaprio    Kristen stewart
+              Maroon 5           Jennifer lopez   (Katy perry)      (Billie eilish)   (Camila cabello)    Tobey maguire        Emma stone
+                                 optional(8)      (Beyonce)         (Selena gomez)                        Andrew Garfield
+                                 
+                                 
+Canada singers - Male                                 
+                 Justin Bieber                 
+                                 
+                                 
+Additional songs - Despacito              Desiigner's panda
+                   My heart will go on    Beat it
+                   Serena's safari        White town's your women
+                   
