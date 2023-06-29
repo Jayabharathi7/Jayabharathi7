861f05ab9fdf74b18f03db0a45bcd979579221ef
JAYABHARATHI J <101325443+Jayabharathi7@users.noreply.github.com>
2023-06-29 12:43:04 +0530

    Update makefile.c

--- TEMPLATECODES/makefile.c
+++ TEMPLATECODES/makefile.c
@@ -16,6 +16,74 @@ clean:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Updates upto 2023
+
+Top Microcontrollers:
+PIC           STM       Beaglebone   ARM          ATtiny(AVR)    Microchip(AVR, ATmega, PIC)
+CC3235MODSF   Atmel     Cortex       AVR          Zilog          TI(MSP)
+8051          Rpi       MSP          Intel 8051   Renesas        Holtek
+ESP32	      Arduino   Teensy       NXP LPC      TI
+	
+
+Top Tools:
+Keil      MPLAB         
+MATLAB    CCS
+Ubuntu    SDK
+IDE       Arduino IDE
+
+
+Acadamics used tools:
+IDE, MATLAB, LABORATORY(Basic Hardware related in bread board), 8086 Development Kits, 8086 Assembler, 8051 Cross Assembler,
+Microcontroller, Xilinx, FPGA Boards, Embedded trainer kits with ARM Board, MASM(Macro Assembler)
+
+
+Acadamics skills learned:
+Python, Circuit lab, C, Data Structures, Analog and Digital Circuits, Linear IC lab, DSP lab, Network lab, Communication System lab,
+VLSI, Microcontroller and Microprocessor, Embedded lab, Hardware
+
+
+Top Operating Systems:	
+Windows    Linux Kernel   Chrome_OS
+Android	   Ubuntu         Apple_ios
+Fedora     Linux          Debian
+MAC_OS     Unix           Blackberry
+
+
+
+	
 	
 
 
