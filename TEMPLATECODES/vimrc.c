71335451360b1ad257a37f973477eb270de263c4
JAYABHARATHI J <101325443+Jayabharathi7@users.noreply.github.com>
2024-08-23 11:16:45 +0530

    Update vimrc.c

--- TEMPLATECODES/vimrc.c
+++ TEMPLATECODES/vimrc.c
@@ -113,6 +113,76 @@ FA Cup Trophy                 UEFA European Championship
 
 Leagues:   1.Bundesliga     2.Premier League     3.Serie A      4.La Liga      5.Ligue 1       6.Primeira Liga
 
+Drinks:
+3 Types of Alhocol:
+Methanol    - Fuel for cars and boats.Used to manufacture antifreeze,paint remover,windshield wiper fluid and many other products.
+Ispropanol  - Chemical name for rubbing alcohol. Used for cleaning and disinfecting.
+Both methanol and isopropanol are poisonous to humans because our bodies metabolize them as toxic substances which cause liver failure. 
+Drinking even a small amount of methanol or rubbing alcohol can be fatal.
+
+Ethanol     - Ethanol (or ethyl alcohol) is the type of alcohol,people drinks.
+
+Undistilled Drinks:        Distilled Drinks:(Liquors and Spirits)
+Beer         Mead          Gin       Rum         Absinthe     Schnapps
+Wine         Sake          Brandy    Tequila     Everclear                    
+Hard Cider                 Whiskey   Vodka       Liqueurs             
+                                             
+Healthy Foods:
+Apples            Eggs              Coconuts          Broccoli      Onions     Trout          Homemade Low-Carb and    Cheese
+Avocados          Lean beef         Macadamia Nuts    Carrots       Tomatoes   Tuna           Gluten-free Breads       Dairy Milk
+Bananas           Chicken Breasts   Brazil Nuts       Cauliflower   Salmon     Brown Rice     Green Beans              Yogurt
+Blueberries       Lamb and Mutton   Walnuts           Cucumber      Sardines   Oats           Kidney Beans             Extra-Virgin Olive Oil
+Oranges           Almonds           Asparagus         Garlic        Shellfish  Quinoa         Lentils                  Coconut Oil
+Strawberries      Chia Seeds        Bell Peppers      Kale          Shrimp     Ezekiel Bread  Peanuts                  Potatoes
+
+Sweet Potatoes           Grains            Sports Drinks and Energy Beverages    Some Vegetable Oils                Kiwi          Chard 
+Apple Cider Vinegar      Savory Snacks     Granola and Granola Bars              Premade Smoothies                  Barley        Bulgur
+Dark chocolate           Sweet Desserts    Gluten-free Snack Foods               Diet Soda                          Dates         Whole-grain Pasta
+Sauce Condiment          Fats and Oils     Some Low Fat and Fat-free Products    Some Plant-based Meat Products     Pumpkin       Black Beans
+Mixed Dishes             Meats             Breakfast Cereal                      Mushrooms                          Pomagranates  Spinach
+Beverages                Water             Soy                                   Edamame                            Nut Butter    Turmeric
+
+Dark Green Leafy Vegetables               Cinnamon      Black Berries        Gouda
+Bivalves such as Oysters and Clams        Raisin        Complan,Horlicks     Kefir
+Crustaceans such as Lobster and Shrimp    Badam Mix     Pickles              Liver
+Proteins for Fitness                      Cashew Nuts   Rusk                 Tempeh
+Energy Mixes with Milk                    Lemons        Wheat Germ           Watercress
+Biscuits - Unibic, Nutrichoice,           Watermelon    Blackcurrants
+Farmlite Digestive.                       Pistachio     Brussels Sprouts
+
+Household Items:
+Laptop         Himalaya -Soap,Shampoo,Paste,Brush        Pain Balm,Bandage,Inhaler
+Mobile         Notebook,Gel Pen, Daily Learn New Things  Dumbells,Skipping Ropes and
+Axe,Lynx,Fogg  Watch,Chain,Glass,Trimmer,Headset         other things.
+
+Change depend upon places.
+Repeat the updates everytime.   
+
+India status: 
+OrderWise:
+HDI Ranking:         Literacy Rate:        Best cities to live:               Best Traditions,Festivals,Lifestyles:
+Kerala               Kerala	             Bangalore         Hyderabad        Rajasthan            Bihar
+Goa	               Delhi                 Chennai           Mumbai           Punjab               Uttarakhand
+Chandigarh	         Uttarakhand           Ahmedabad         Kolkata          Gujaraj              Orissa
+Delhi	               Himachal Pradesh      Chennai           Delhi            Tamil Nadu           Kerala
+Puducherry	         Assam                 Surat                              West Bengal          Madhya Pradesh
+Lakshadweep	         Maharashtra           Navi Mumbai                        Maharastra
+Himachal Pradesh     Punjab                Coimbatore                         Karnataka
+Sikkim               Tamil Nadu            Vadodara                           Uttar Pradesh
+Jammu and Kashmir    Gujarat               Indore                             Himachal Pradesh
+Punjab               West Bengal           Greater Mumbai                     Haryana
+   
+Cummunications:
+Try all the things:    MOVAFFK
+Compared to MOVAFFK, Half way watching is better,but don't cross the limit(Less time and less show). Because having high energy,good feel,
+long time and have no problems.   
+Don't use more than one drop, and use only if you have completely.
+
+Keep in Mind:
+Change depend upon places(Lifestyle,respect,growth....). In there try to become best and change the environment.
+
+Note:
+Repeat the updates everytime.
 
 
 
@@ -132,8 +202,7 @@ Leagues:   1.Bundesliga     2.Premier League     3.Serie A      4.La Liga      5
 
 
 
-
-
+   
 
 
 
