3a406acd18f759dbe1521e046bcd7689c27c8780
JAYABHARATHI J <101325443+Jayabharathi7@users.noreply.github.com>
2025-08-10 12:31:11 +0530

    Update vimrc.c

--- TEMPLATECODES/vimrc.c
+++ TEMPLATECODES/vimrc.c
@@ -155,8 +155,10 @@ Fashion:
     All the dresses before buy, always check the size,color,cloth,design and fittings with the previously used one. Only change the size accordingly if your
     body shape change or don't. If any one of those above mentioned is not matching in required form, don't buy or move to other shop or buy later once stack get
     filled. Buy everythings with Ready-made and don't get tailor-stitched kind(Buying clothes and stitching).
-    All the hem, cuff and other parts need to be in same size. Always wear and check the dress is perfect or not, then buy it.
-   
+    All the hem, cuff and other parts need to be in same size. Always wear and check the dress is perfect or not, then buy it. In every clothes both sides need to
+    same cloth, because in some clothes outside looks good and inside they gave with cheap or worst cloth. So check both sides and need to have same cloth. In
+    shirts inside also need to have same cloth, color, design.
+
 PG/Hostel/Home:    Country ->   City/Town/Village ->   1.Area   2.Environment   3.Culture
 
 Football:
